9b30a4a408ec23a8361af12841f3084a2b2294e1
Jane Lange <janelange15@gmail.com>
2017-12-20 18:40:36 -0800

    added STAN code

--- Code/make_data.R
+++ Code/make_data.R
@@ -1,11 +1,12 @@
-source("/Users/janelange/Documents/Work/Stan/Code/simulatedata.R")
+setwd("/Users/jlange/Documents/Stan/Stan/CTMC-Stan/Code")
+source("simulatedata.R")
 
 rate_mat=matrix(0,nrow=5,ncol=5)
 rate_mat[1,2]=.034
 rate_mat[1,5]=.06
-rate_mat[2,3]=.1
+rate_mat[2,3]=.15
 rate_mat[2,5]=.06
-rate_mat[3,4]=.3
+rate_mat[3,4]=.35
 rate_mat[3,5]=.06
 diag(rate_mat)=(-1)*apply(rate_mat,1,"sum")
 
@@ -15,5 +16,7 @@ out=get.obs.data.many(rate.matrix=rate_mat,obs.times=seq(0,10,.5),end.time=100, 
 simdata=out[[2]];
 outdata=lapply(simdata,FUN="process_data")
 simdata=do.call(rbind, outdata)
+
+
 
 
